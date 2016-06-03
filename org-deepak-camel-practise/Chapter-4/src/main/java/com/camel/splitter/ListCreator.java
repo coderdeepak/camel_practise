33509eebf2ad59ea95c1437e1eec98feeb6f8e61
Deepak <er.deepak16@gmail.com>
2016-06-03 19:54:53 +0530

    Splitter.

---
+++ org-deepak-camel-practise/Chapter-4/src/main/java/com/camel/splitter/ListCreator.java
@@ -0,0 +1,16 @@
+package com.camel.splitter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListCreator {
+	
+	public List<String> getFruits(){
+		List<String> fruits = new ArrayList<String>();
+		fruits.add("apple");
+		fruits.add("pineapple");
+		fruits.add("banana");
+		return fruits;
+	}
+
+}
